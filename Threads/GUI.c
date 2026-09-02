95e05ed9b452d1467dff2b8d8ab60f77b46ebf7c
conmeomaucam <congtran400@gmail.com>
2026-09-02 14:28:53 +0700

    feat: implement GUI thread worker and define communication structures for dashboard and input management

---
+++ Threads/GUI.c
@@ -0,0 +1,12 @@
+/**
+ * @file       GUI.c
+ * @brief      
+ * @author     Cong <congvmc1@gmail.com>
+ * @date       2026-09-02
+ * @version    2.0.0
+ * 
+ * @copyright  Copyright (c) 2026 Cong. All rights reserved.
+ */
+
+
+
